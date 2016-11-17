98d162db88f2c2036eb3f9b07b7347071ba2369b
SimonIlic <simon.ilic@student.uva.nl>
2016-11-17 22:08:25 +0100

    Initial commit

---
+++ app/src/main/java/mprog/simon/simonilic_pset3/FetchMovieData.java
@@ -0,0 +1,93 @@
+package mprog.simon.simonilic_pset3;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+/**
+ * Created by Simon on 15-11-2016.
+ */
+
+public class FetchMovieData extends AsyncTask<String, Void, String>{
+
+    public AsyncResponse delegate = null;
+    private Activity mActivity;
+
+    public FetchMovieData(Activity activity, AsyncResponse delegate){
+        mActivity = activity;
+        this.delegate = delegate;
+    }
+
+    // Interface to relay data back to MainActivity
+    public interface AsyncResponse {
+        void processFinish(JSONObject output);
+    }
+
+    protected void onPreExecute() {
+        Toast.makeText(mActivity.getApplicationContext(),
+                "Fetching data from server", Toast.LENGTH_SHORT).show();
+    }
+
+
+    @Override
+    protected String doInBackground(String... args) {
+
+        String api_url = "http://www.omdbapi.com/?t=";
+
+        URL url = null;
+        try {
+            url = new URL(api_url + args[0]);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            try {
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                StringBuilder stringBuilder = new StringBuilder();
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(line).append("\n");
+                }
+                bufferedReader.close();
+                return stringBuilder.toString();
+            } finally {
+                urlConnection.disconnect();
+            }
+        } catch (Exception e) {
+            Log.e("ERROR", e.getMessage(), e);
+            Toast.makeText(mActivity, "Error fetching data", Toast.LENGTH_SHORT).show();
+            return null;
+        }
+    }
+
+    protected void onPostExecute(String result) {
+        if (result == null) {
+            return;
+        }
+
+        Toast.makeText(mActivity.getApplicationContext(), "Fetched data",
+                Toast.LENGTH_SHORT).show();
+
+        JSONObject resultJSON = null;
+        try {
+            resultJSON = new JSONObject(result);
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        delegate.processFinish(resultJSON);
+
+
+    }
+}
---
+++ app/src/main/java/mprog/simon/simonilic_pset3/MainActivity.java
@@ -0,0 +1,129 @@
+package mprog.simon.simonilic_pset3;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity implements FetchMovieData.AsyncResponse {
+
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void search(View view) {
+        // get seached title
+        EditText editTextView = (EditText) findViewById(R.id.searchField);
+        String search_term = editTextView.getText().toString();
+
+        // create asyncTask class instance
+        FetchMovieData asyncTask = new FetchMovieData(this, this);
+        asyncTask.execute(search_term);
+    }
+
+    //this override the implemented method from AsyncResponse
+    @Override
+    public void processFinish(JSONObject output){
+
+        Toast.makeText(this, "YAY", Toast.LENGTH_SHORT).show();
+
+        // Get ListView object from xml
+        listView = (ListView) findViewById(R.id.list);
+
+        // Defined Array values to show in ListView
+        String[] values = new String[] { "Android List View",
+                "Adapter implementation",
+                "Simple List View In Android",
+                "Create List View Android",
+                "Android Example",
+                "List View Source Code",
+                "List View Array Adapter",
+                "Android Example List View"
+        };
+
+        // Define a new Adapter
+        // First parameter - Context
+        // Second parameter - Layout for the row
+        // Third parameter - ID of the TextView to which the data is written
+        // Forth - the Array of data
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+
+
+        // Assign adapter to ListView
+        listView.setAdapter(adapter);
+
+        // ListView Item Click Listener
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                // ListView Clicked item index
+                int itemPosition     = position;
+
+                // ListView Clicked item value
+                String  itemValue    = (String) listView.getItemAtPosition(position);
+
+                // Show Alert
+                Toast.makeText(getApplicationContext(),
+                        "Position :"+itemPosition+"  ListItem : " +itemValue , Toast.LENGTH_LONG)
+                        .show();
+
+
+
+            }
+
+        });
+    }
+}
